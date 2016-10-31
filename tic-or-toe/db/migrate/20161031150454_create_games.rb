0135ae0daabfeb6534fb2487e260494fd4a5ad94
Olivia Coffaro <email": null>
2016-10-31 11:13:54 -0400

    created user and game models

--- tic-or-toe/db/migrate/20161031150454_create_games.rb
+++ tic-or-toe/db/migrate/20161031150454_create_games.rb
@@ -1,7 +1,10 @@
 class CreateGames < ActiveRecord::Migration[5.0]
   def change
     create_table :games do |t|
-      
+      t.integer :user_id
+      t.boolean :game_won
+      t.text :game_board, array: true, default: []
+
       t.timestamps
     end
   end
