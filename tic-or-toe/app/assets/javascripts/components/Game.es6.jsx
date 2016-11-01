c344f08b2ccce470a2b13c1e35c64e07b1c581fa
Matt Edwards <email": null>
2016-11-01 10:52:04 -0400

    add 3d css to logo, change grid to 5x5

--- tic-or-toe/app/assets/javascripts/components/Game.es6.jsx
+++ tic-or-toe/app/assets/javascripts/components/Game.es6.jsx
@@ -3,37 +3,48 @@ class Game extends React.Component {
 
   render(){
 
-      return(
-        <section className="container">
-        <div id="board">
-    <table>
-    <tbody>
+    return(
+      <div id="board">
+      <table>
+      <tbody>
       <tr id="row1">
-        <td id="zero-zero" className="square"></td>
-        <td id="zero-one" className="square"></td>
-        <td id="zero-two" className="square"></td>
+      <td className="square"></td>
+      <td className="square"></td>
+      <td className="square"></td>
+      <td className="square"></td>
+      <td className="square"></td>
       </tr>
       <tr id="row2">
-        <td id="one-zero" className="square"></td>
-        <td id="one-one" className="square"></td>
-        <td id="one-two" className="square"></td>
+      <td className="square"></td>
+      <td className="square"></td>
+      <td className="square"></td>
+      <td className="square"></td>
+      <td className="square"></td>
       </tr>
       <tr id="row3">
-        <td id="two-zero" className="square"></td>
-        <td id="two-one" className="square"></td>
-        <td id="two-two" className="square"></td>
+      <td className="square"></td>
+      <td className="square"></td>
+      <td className="square"></td>
+      <td className="square"></td>
+      <td className="square"></td>
+      </tr>
+      <tr id="row4">
+      <td className="square"></td>
+      <td className="square"></td>
+      <td className="square"></td>
+      <td className="square"></td>
+      <td className="square"></td>
+      </tr>
+      <tr id="row5">
+      <td className="square"></td>
+      <td className="square"></td>
+      <td className="square"></td>
+      <td className="square"></td>
+      <td className="square"></td>
       </tr>
       </tbody>
-    </table>
-  </div>
-        </section>
-      )
-    }
-
-
-    $(".square").on(click, )
-
-
-
-
+      </table>
+      </div>
+    )
+  }
 }
