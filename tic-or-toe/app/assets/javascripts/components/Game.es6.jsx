59bc133478d3c9242cb59e1e6056a65af69ad3dd
Matt Edwards <email": null>
2016-11-01 23:55:16 -0400

    Connect logic to game

--- tic-or-toe/app/assets/javascripts/components/Game.es6.jsx
+++ tic-or-toe/app/assets/javascripts/components/Game.es6.jsx
@@ -1,25 +1,51 @@
 class Game extends React.Component {
   constructor() {
     super()
+    this.state = {
+      board: [],
+      gameID: ''
+    }
   }
 
-  // updateBoard(square) {
-  //   var board = this.state.board
-  //   // find the spot in the array, and update it with the square
-  //   this.setState(board: [newArray])
-  // }
+  componentWillMount(){
+    this.setState({board: this.props.rubyBoard, gameID: this.props.gameID})
+  }
 
+  updateBoard(new_board) {
+    this.setState({board: new_board})
+  }
   render(){
-    const squares = this.props.rubyBoard.map(square => {
-      return(<Square key={square.id} data={square.content} />)
-    })
+    var parent = this
+    var parsedRow = function(row){
+      return row.map(square => {
+        return(<Square parent={parent} key={square.id} objKey = {square.id} data={square.content} />)
+      })
+    }
+
+    var row1Return = parsedRow(this.state.board.slice(0, 5))
+    var row2Return = parsedRow(this.state.board.slice(5, 10))
+    var row3Return = parsedRow(this.state.board.slice(10, 15))
+    var row4Return = parsedRow(this.state.board.slice(15, 20))
+    var row5Return = parsedRow(this.state.board.slice(20, 25))
+
     return(
-      <div className="row">
-        {squares}
+      <div className="board">
+        <div className="row">
+          {row1Return}
+        </div>
+        <div className="row">
+          {row2Return}
+        </div>
+        <div className="row">
+          {row3Return}
+        </div>
+        <div className="row">
+          {row4Return}
+        </div>
+        <div className="row">
+          {row5Return}
+        </div>
       </div>
     )
   }
 }
-
-
-// {board.map(sq => <Square updateBoard={this.updateBoard} marked={sq} />)}
--- tic-or-toe/app/assets/javascripts/components/Square.es6.jsx
+++ tic-or-toe/app/assets/javascripts/components/Square.es6.jsx
@@ -1,8 +1,20 @@
 class Square extends React.Component {
-
+  viewMore(){
+    var gameBoard = this.props.parent
+    var route = "/games/"+gameBoard.props.gameID+'/edit'
+    $.ajax({
+      url: route,
+      method: 'get',
+      data: {
+              player_move: this.props.objKey
+            }
+    }).done((response)=>{
+      gameBoard.updateBoard(response.board)
+    })
+  }
   render(){
     return(
-      <div className="square col s2">
+      <div className="red-text square col s2" onClick={this.viewMore.bind(this)}>
         {this.props.data}
       </div>
     )
